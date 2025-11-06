6ca6b6ed1360fbece8c01b5efe42faa435f3e164
RodolfoJGonz <rodgonz0322@gmail.com>
2025-11-06 10:31:54 -0600

    Working on filtering outputs

--- main/main.py
+++ main/main.py
@@ -1,34 +1,32 @@
 from ultralytics import YOLO
-import cv2 as cv
-# import numpy as np
+from ultralytics.engine.results import Results
+import torch
+import cv2
+import numpy as np
+
+
+max_per_class = {
+    0: 2,  # black-bishop
+    1: 1,  # black-king
+    2: 2,  # black-knight
+    3: 8,  # black-pawn
+    4: 1,  # black-queen
+    5: 2,  # black-rook
+    6: 2,  # white-bishop
+    7: 1,  # white-king
+    8: 2,  # white-knight
+    9: 8,  # white-pawn
+    10: 1,  # white-queen
+    11: 2,  # white-rook
+}
+
+
+def filter(classes, conf, max_per_class=max_per_class):
+    return
 
 
 def run():
-    ### TRAINING ###
-    # We will probably use 1 GPU for now from the cluster #
-
-    model = YOLO("yolo11n.yaml")
-
-    """
-    Some parameters to keep in mind:
-    workers: Number of worker threads for data loading (Default == 8)
-    project: Name of directory where to store training outputs
-    name: Name the training run. For creating subdirectory withing project folder
-    multi_scale: Enables multi_scale training by increasing/decreasing imgsz by 0.5 during training
-    warmup_epochs: Look it up
-    val: Validation during training, allowing periodic eval of model performance on separate dataset
-    plots: Generates and plots for examining
-    """
-    results = model.train(
-        data="../Chess Pieces 2.v5i.yolov11/data.yaml",
-        epochs=10,
-        device=0,  # Uses GPU
-        project="./results",
-        name="test_run",
-        multi_scale=True,
-        plots=True,
-    )
-    print(results)
+    model = YOLO("../models/pieces/best.pt")
 
     ###Webcam Tracking###
     # results = model.track(source=0, stream=True, show=True)
@@ -38,11 +36,71 @@ def run():
     #    probs = r.probs
 
     ###Picture Testing###
-    # results = model(
-    #    "./IMG_20230323_065255_139_jpg.rf.e3b232dad7b4f84f957336b414f1f143.jpg"
+    # results = model.predict(
+    #    source="../Videos/IMG_3109.jpeg",
+    #    # conf=0.45,
+    #    iou=0.40,
     # )
-    # results[0].show()
-    return
+    # for r in results:
+    #    r.show()
+    # return
+
+    ### Video Testing ###
+
+    video_path = "../Videos/IMG_3114.mov"
+    cap = cv2.VideoCapture(video_path)
+    ## Loop through the video frames
+    while cap.isOpened():
+        # Read a frame from the video
+        success, frame = cap.read()
+        if not success:
+            break
+
+        resize = cv2.resize(frame, (1920, 1080))
+        results = model.predict(resize, conf=0.4, iou=0.4, verbose=False)
+
+        boxes = results[0].boxes
+        classes = boxes.cls.cpu().numpy().astype(int)
+        conf = boxes.conf.cpu().numpy()
+        xyxy = boxes.xyxy.cpu().numpy()
+
+        # Filtering only max amount
+        keep_idx = []
+        for c in np.unique(classes):
+            idx = np.where(classes == c)[0]
+            sorted_idx = idx[np.argsort(-conf[idx])]
+            n_keep = max_per_class.get(c, len(sorted_idx))
+            keep_idx.extend(sorted_idx[:n_keep])
+
+        keep_idx = np.array(keep_idx)
+        cls_filtered = classes[keep_idx]
+        conf_filtered = conf[keep_idx]
+        xyxy_filtered = xyxy[keep_idx]
+
+        boxes_tensor = torch.tensor(
+            np.concatenate(
+                [xyxy_filtered, conf_filtered[:, None], cls_filtered[:, None]], axis=1
+            ),
+            dtype=torch.float32,
+        )
+
+        filtered = Results(
+            orig_img=resize,
+            path="",
+            boxes=boxes_tensor,
+            names=model.names,
+        )
+
+        annotated_frame = filtered.plot()
+        # print(np.unique(cls_filtered, return_counts=True))
+        cv2.imshow("Filtered Stream", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    ## Release the video capture object and close the display window
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
